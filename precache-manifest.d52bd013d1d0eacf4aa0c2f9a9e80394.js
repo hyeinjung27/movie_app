35af3bf80c005323626f496b2e07f27b33e8ea97
hyein <hyeinjung99@naver.com>
2020-10-22 13:34:26 +0900

    Updates

---
+++ precache-manifest.d52bd013d1d0eacf4aa0c2f9a9e80394.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d19fcdd4a136ce02059476a6bf15bba3",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "a1a92e548474a14467c5",
+    "url": "/movie_app/static/css/main.fa249032.chunk.css"
+  },
+  {
+    "revision": "2cb00f0b337cb1b1485d",
+    "url": "/movie_app/static/js/2.03729dac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.03729dac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1a92e548474a14467c5",
+    "url": "/movie_app/static/js/main.b81ae1b1.chunk.js"
+  },
+  {
+    "revision": "8a294428b8499a7b0c17",
+    "url": "/movie_app/static/js/runtime-main.9ac0a6e2.js"
+  }
+]);
